b7352979e24278a6359aeaca3c7fc82b9d8f2e59
vishnurrao <vishnuparashara@gmail.com>
2026-03-28 13:32:21 +0530

    log additions

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5173", 
         "http://localhost:5003",
         "http://127.0.0.1:5003",
     ],
